5bddf00efefc9c270b498e47277d1ca7ffe195cb
fajaragungpramana <fajar.agungpramana01@gmail.com>
2022-11-27 22:40:13 +0700

    Create beta horizontal bar chart.

--- app/src/main/java/com/github/fajaragungpramana/example/MainActivity.kt
+++ app/src/main/java/com/github/fajaragungpramana/example/MainActivity.kt
@@ -1,13 +1,23 @@
 package com.github.fajaragungpramana.example
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.github.fajaragungpramana.chart.view.HorizontalBarChart
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val horizontalBarChart = findViewById<HorizontalBarChart>(R.id.hbc_view)
+
+        val percentages = arrayListOf(50, 30)
+        val backgrounds = arrayListOf(Color.RED, Color.YELLOW)
+
+        horizontalBarChart.submitPercentage(percentages)
+        horizontalBarChart.submitBackground(backgrounds)
     }
 
 }
--- chart/src/main/java/com/github/fajaragungpramana/chart/view/HorizontalBarChart.kt
+++ chart/src/main/java/com/github/fajaragungpramana/chart/view/HorizontalBarChart.kt
@@ -2,12 +2,48 @@ package com.github.fajaragungpramana.chart.view
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.View
 import android.widget.LinearLayout
 
 class HorizontalBarChart(context: Context, attrs: AttributeSet) : LinearLayout(context, attrs) {
 
+    private val mListChartPercentage by lazy { arrayListOf<Int>() }
+    private val mListChartBackground by lazy { arrayListOf<Int>() }
+
     init {
         orientation = HORIZONTAL
+    }
+
+    fun submitPercentage(data: List<Int>) {
+        mListChartPercentage.clear()
+        mListChartPercentage.addAll(data)
+    }
+
+    fun submitBackground(data: List<Int>) {
+        mListChartBackground.clear()
+        mListChartBackground.addAll(data)
+    }
+
+    override fun onWindowFocusChanged(hasWindowFocus: Boolean) {
+        super.onWindowFocusChanged(hasWindowFocus)
+        removeAllViews()
+
+        if (mListChartPercentage.isNotEmpty()) {
+            for (i in mListChartPercentage.indices) {
+
+                val bar = View(context)
+                val barWidth = (width * mListChartPercentage[i]) / 100
+
+                if (mListChartBackground.isNotEmpty() && mListChartBackground.size <= mListChartPercentage.size)
+                    bar.setBackgroundColor(mListChartBackground[i])
+
+                bar.layoutParams = LayoutParams(barWidth, LayoutParams.MATCH_PARENT)
+
+                addView(bar)
+
+            }
+        }
+
     }
 
 }
