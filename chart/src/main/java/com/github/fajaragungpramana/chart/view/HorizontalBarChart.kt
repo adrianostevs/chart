52cc10f7fd8ad37189d9a2263dabb47e9afea5ee
fajaragungpramana <fajar.agungpramana01@gmail.com>
2022-11-27 21:39:14 +0700

    Create activity for example.

---
+++ chart/src/main/java/com/github/fajaragungpramana/chart/view/HorizontalBarChart.kt
@@ -0,0 +1,13 @@
+package com.github.fajaragungpramana.chart.view
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.LinearLayout
+
+class HorizontalBarChart(context: Context, attrs: AttributeSet) : LinearLayout(context, attrs) {
+
+    init {
+        orientation = HORIZONTAL
+    }
+
+}
